ae7147f255025862aa6de3b30a09117f621e029b
arbitrata <andrewmbugua76@gmail.com>
2023-05-09 09:32:43 +0300

    Modification on the site routing

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -4,13 +4,13 @@ import Button from "../common/Button";
 
 export const NotFound = () => {
   return (
-    <div class="h-screen w-full bg-lightgray flex items-center">
-      <div class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
-        <div class="w-full lg:w-1/2 mx-8">
-          <div class="text-7xl text-green-500 font-dark font-extrabold mb-8">
+    <div className="h-screen w-full bg-lightgray lg:flex items-center">
+      <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
+        <div className="w-full lg:w-1/2 mx-8">
+          <div className="text-7xl text-green-500 font-dark font-extrabold mb-8">
             404
           </div>
-          <p class="text-2xl md:text-3xl font-poppins leading-normal mb-8">
+          <p className="text-2xl md:text-3xl font-poppins leading-normal mb-8">
             Sorry we couldn't find the page you're looking for
           </p>
           <Navigate to="/not-found" replace={true}>
@@ -24,10 +24,10 @@ export const NotFound = () => {
             />
           </Link>
         </div>
-        <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+        <div className="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
           <img
             src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg"
-            class=""
+            className=""
             alt="Page not found"
           />
         </div>
